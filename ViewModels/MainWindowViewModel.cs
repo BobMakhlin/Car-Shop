bce032db4171ddaac0b2eabc767c8e8534d2300a
Boris Makhlin <borismakhlin5@gmail.com>
2020-01-17 20:18:50 +0200

    Show cars in the ListBox at left of the program window

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CarsShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarsShop.ViewModels
+{
+    class MainWindowViewModel
+    {
+        #region Private Definitions
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            Cars = new CarsStorage();
+        }
+
+        public ObservableCollection<Car> Cars { get; private set; }
+
+        #region INotifyPropertyChanged
+
+        #endregion
+    }
+}
